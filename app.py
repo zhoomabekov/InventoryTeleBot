d2d88992b5a5995530b6d070c1b988f604cc7598
zhoomabekov <gzhumabekov@nu.edu.kz>
2023-06-06 01:00:43 +0600

    Multiple matches
    Now the bot can handle multiple inventory number matches. In case there are more than 1 matches, the bot send all the matches in a form of buttons.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 from config import TOKEN
 from extensions import APIException
 import csv
@@ -24,13 +25,6 @@ def lookup_values_list(data, key):
     return matches
 
 
-def lookup_value(data, key):
-    for row in data:
-        if key in row['Инвентарный номер']:
-            print(row['Инвентарный номер'], row['Основное средство'])
-            return row
-
-
 # Example usage
 table_data = load_data('List.csv')
 
@@ -40,46 +34,49 @@ def handle_message_2(message):
     user_message = message.text.upper()
     if len(user_message) <= 9:
         results = lookup_values_list(table_data, user_message)
-        if results:
-            reply_message = "\n".join([i['Инвентарный номер'] for i in results])
-            print(reply_message)
+        if len(results) == 1:
+            lookup_value(table_data, results[0]['Инвентарный номер'], message)
+        elif len(results) > 1:
+            matching_inv_numbers = [i['Инвентарный номер'] for i in results]
+
+            keyboard = types.InlineKeyboardMarkup(row_width=1)
+            for i in matching_inv_numbers:
+                button = types.InlineKeyboardButton(text=i, callback_data=i)
+                keyboard.add(button)
+            bot.send_message(message.chat.id, "Which one?", reply_markup=keyboard)
+
         else:
-            reply_message = 'Entry not found in the SEDS IT equipment list'
+            bot.reply_to(message, 'Entry not found in the SEDS IT equipment list', parse_mode='Markdown')
     else:
         reply_message = "The inventory number (or its part) should not be more than 9 symbols long"
+        bot.reply_to(message, reply_message, parse_mode='Markdown')
 
-    bot.reply_to(message, reply_message, parse_mode='Markdown')
+
+@bot.callback_query_handler(func=lambda call: True)
+def handle_callback_query(call):
+    lookup_value(table_data, call.data, call.message)
 
 
-# @bot.message_handler(func=lambda message: True)
-def handle_message(message):
-    user_message = message.text.upper()
-    if len(user_message) <= 9:
-        result = lookup_value(table_data, user_message)
-        if result:
+def lookup_value(data, key, message):
+    for row in data:
+        if key == row['Инвентарный номер']:
             reply_message = f'''
-Дата передачи: {result['Дата передачи']}
+Дата передачи: {row['Дата передачи']}
 
-*{result['Инвентарный номер']}*
-{result['Основное средство']}
+*{row['Инвентарный номер']}*
+{row['Основное средство']}
+{row['Местоположение']}
 
-{result['Местоположение']}
+{row['Пользователь']}
+{row['Должность']}
 
-{result['Пользователь']}
-{result['Должность']}
+{row['Первоначальная стоимость']} KZT
+Дата поступления: {row['Дата поступления']}
+Примечание: {row['Примечание']}
 
-{result['Первоначальная стоимость']} KZT
-Дата поступления: {result['Дата поступления']}
-
-Примечание: {result['Примечание']}
+Актуальность данных: {row['АКТУАЛЬНОСТЬ']}
 '''
-        else:
-            reply_message = 'Entry not found in the SEDS IT equipment list'
-    else:
-        reply_message = "The inventory number (or its part) should not be more than 9 symbols long"
-
-    bot.reply_to(message, reply_message, parse_mode='Markdown')
+            bot.send_message(message.chat.id, reply_message, parse_mode='Markdown')
 
 
-# Start the bot
 bot.polling()
